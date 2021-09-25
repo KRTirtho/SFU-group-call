e55d018068a971627e1e0b2f2ef286b82c556e1e
KRTirtho <krtirtho@gmail.com>
2021-09-25 13:25:18 +0600

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# SFU Group Call
+A video conference web-app that is super fast & efficient
+It uses **mediasoup** under the hood to provide fastest video conference experience. It has capability to broadcast for up to 50 participants simultaneously where traditional video-conferences are limited to 25-30 participants
+
+Mediasoup is a SFU (Selective Forwarding Unit) WebRTC server/client that is way faster & efficient than MCU (Multipoint Conferencing Unit)/Mesh WebRTC servers
+
+If properly implemented & enough resources provided (4 core CPU & 8GB RAM recommended) it can handle 500 producer/consumer connection simultaneously
+
+But it has a drawback of using huge amounts of ports. Minimum port usage is around 100
+
+By using **mediasoup** for this application, this can be easily said that **SFU-group-call** is yet the fastest & smoothest video conferencing application demo along with other application project created with mediasoup 
---
+++ server/app.ts
@@ -0,0 +1,266 @@
+/**
+ * integrating mediasoup server with a node.js application
+ */
+
+/* Please follow mediasoup installation requirements */
+/* https://mediasoup.org/documentation/v3/mediasoup/installation/ */
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import {createWorker} from "mediasoup";
+import { Worker } from "mediasoup/lib/Worker";
+import { Router } from "mediasoup/lib/Router";
+import { Producer } from "mediasoup/lib/Producer";
+import { Consumer } from "mediasoup/lib/Consumer";
+import { RtpCodecCapability } from "mediasoup/lib/RtpParameters";
+import { WebRtcTransport } from "mediasoup/lib/WebRtcTransport";
+
+const app = express();
+app.get("/", (_, res) => {
+  res.send("Hello from mediasoup app!");
+});
+
+const httpsServer = http.createServer(app);
+httpsServer.listen(4000, () => {
+  console.log("listening on port: " + 4000);
+});
+
+const io = new Server(httpsServer, {transports: ["websocket"]});
+
+// socket.io namespace (could represent a room?)
+const peers = io.of("/mediasoup");
+
+/**
+ * Worker
+ * |-> Router(s)
+ *     |-> Producer Transport(s)
+ *         |-> Producer
+ *     |-> Consumer Transport(s)
+ *         |-> Consumer
+ **/
+let worker: Worker;
+let router: Router;
+let producerTransport: WebRtcTransport;
+let consumerTransport: WebRtcTransport;
+let producer: Producer;
+let consumer: Consumer;
+
+const createSoupWorker = async () => {
+  worker = await createWorker({
+    rtcMinPort: 2000,
+    rtcMaxPort: 2020,
+  });
+  console.log(`worker pid ${worker.pid}`);
+
+  worker.on("died", (error) => {
+    // This implies something serious happened, so kill the application
+    console.error("mediasoup worker has died: ", error);
+    setTimeout(() => process.exit(1), 2000); // exit in 2 seconds
+  });
+
+  return worker;
+};
+
+// We create a Worker as soon as our application starts
+createSoupWorker();
+
+// This is an Array of RtpCapabilities
+// https://mediasoup.org/documentation/v3/mediasoup/rtp-parameters-and-capabilities/#RtpCodecCapability
+// list of media codecs supported by mediasoup ...
+// https://github.com/versatica/mediasoup/blob/v3/src/supportedRtpCapabilities.ts
+const mediaCodecs: RtpCodecCapability[] = [
+  {
+    kind: "audio",
+    mimeType: "audio/opus",
+    clockRate: 48000,
+    channels: 2,
+  },
+  {
+    kind: "video",
+    mimeType: "video/VP8",
+    clockRate: 90000,
+    parameters: {
+      "x-google-start-bitrate": 1000,
+    },
+  },
+];
+
+peers.on("connection", async (socket) => {
+  console.log(socket.id);
+  socket.emit("connection-success", {
+    socketId: socket.id,
+  });
+
+  socket.on("disconnect", () => {
+    // do some cleanup
+    console.log("peer disconnected");
+  });
+
+  // worker.createRouter(options)
+  // options = { mediaCodecs, appData }
+  // mediaCodecs -> defined above
+  // appData -> custom application data - we are not supplying any
+  // none of the two are required
+  router = await worker.createRouter({ mediaCodecs });
+
+  // Client emits a request for RTP Capabilities
+  // This event responds to the request
+  socket.on("getRtpCapabilities", (callback) => {
+    const rtpCapabilities = router.rtpCapabilities;
+
+    console.log("rtp Capabilities", rtpCapabilities);
+
+    // call callback from the client and send back the rtpCapabilities
+    callback({ rtpCapabilities });
+  });
+
+  // Client emits a request to create server side Transport
+  // We need to differentiate between the producer and consumer transports
+  socket.on("createWebRtcTransport", async ({ sender }, callback) => {
+    console.log(`Is this a sender request? ${sender}`);
+    // The client indicates if it is a producer or a consumer
+    // if sender is true, indicates a producer else a consumer
+    const transport = await createWebRtcTransport(callback)
+    if (sender && transport) {
+      producerTransport = transport;
+    }
+    else if(!sender && transport){
+      consumerTransport = transport
+    }
+  });
+
+  // see client's socket.emit('transport-connect', ...)
+  socket.on("transport-connect", async ({ dtlsParameters }) => {
+    console.log("DTLS PARAMS... ", { dtlsParameters });
+    await producerTransport.connect({ dtlsParameters });
+  });
+
+  // see client's socket.emit('transport-produce', ...)
+  socket.on("transport-produce", async ({ kind, rtpParameters, appData }, callback) => {
+    // call produce based on the prameters from the client
+    producer = await producerTransport.produce({
+      kind,
+      rtpParameters,
+    });
+
+    console.log("Producer ID: ", producer.id, producer.kind);
+
+    producer.on("transportclose", () => {
+      console.log("transport for this producer closed ");
+      producer.close();
+    });
+
+    // Send back to the client the Producer's id
+    callback({
+      id: producer.id,
+    });
+  });
+
+  // see client's socket.emit('transport-recv-connect', ...)
+  socket.on("transport-recv-connect", async ({ dtlsParameters }) => {
+    console.log(`DTLS PARAMS: ${dtlsParameters}`);
+    await consumerTransport.connect({ dtlsParameters });
+  });
+
+  socket.on("consume", async ({ rtpCapabilities }, callback) => {
+    try {
+      // check if the router can consume the specified producer
+      if (
+        router.canConsume({
+          producerId: producer.id,
+          rtpCapabilities,
+        })
+      ) {
+        // transport can now consume and return a consumer
+        consumer = await consumerTransport.consume({
+          producerId: producer.id,
+          rtpCapabilities,
+          paused: true,
+        });
+
+        consumer.on("transportclose", () => {
+          console.log("transport close from consumer");
+        });
+
+        consumer.on("producerclose", () => {
+          console.log("producer of consumer closed");
+        });
+
+        // from the consumer extract the following params
+        // to send back to the Client
+        const params = {
+          id: consumer.id,
+          producerId: producer.id,
+          kind: consumer.kind,
+          rtpParameters: consumer.rtpParameters,
+        };
+
+        // send the parameters to the client
+        callback({ params });
+      }
+    } catch (error: any) {
+      console.log(error.message);
+      callback({
+        params: {
+          error: error,
+        },
+      });
+    }
+  });
+
+  socket.on("consumer-resume", async () => {
+    console.log("consumer resume");
+    await consumer.resume();
+  });
+});
+
+const createWebRtcTransport = async (callback: (opts: {params: unknown})=>void) => {
+  try {
+    // https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
+    const webRtcTransport_options = {
+      listenIps: [
+        {
+          ip: "0.0.0.0", // replace with relevant IP address
+          announcedIp: "127.0.0.1",
+        },
+      ],
+      enableUdp: true,
+      enableTcp: true,
+      preferUdp: true,
+    };
+
+    // https://mediasoup.org/documentation/v3/mediasoup/api/#router-createWebRtcTransport
+    let transport = await router.createWebRtcTransport(webRtcTransport_options);
+    console.log(`transport id: ${transport.id}`);
+
+    transport.on("dtlsstatechange", (dtlsState) => {
+      if (dtlsState === "closed") {
+        transport.close();
+      }
+    });
+
+    transport.on("close", () => {
+      console.log("transport closed");
+    });
+
+    // send back to the client the following prameters
+    callback({
+      // https://mediasoup.org/documentation/v3/mediasoup-client/api/#TransportOptions
+      params: {
+        id: transport.id,
+        iceParameters: transport.iceParameters,
+        iceCandidates: transport.iceCandidates,
+        dtlsParameters: transport.dtlsParameters,
+      },
+    });
+
+    return transport;
+  } catch (error) {
+    console.log(error);
+    callback({
+      params: {
+        error: error,
+      },
+    });
+  }
+};
